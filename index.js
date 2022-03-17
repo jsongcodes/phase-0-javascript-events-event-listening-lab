8e0b6e7731bbe6c35f424eca3df0d6e8d964212c
Jasmin <ssongnari@gmail.com>
2022-03-17 19:40:10 -0400

    commit tree 97c07a922d27d51c1c039e0fdf392d2242071efa

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+// document.querySelector('alert').addEventListener('click', () => alert('Hi, I was clicked'))
+
+// document.querySelector('#alert').addEventListener('click', addingEventListener)
+
 function addingEventListener() {
+    const input = document.getElementById('input');
+
+    function clickAlert() {
+        alert('I was clicked!');
+    }
+
+    input.addEventListener('click', clickAlert);
 }
+
+const input = document.getElementById('input');
+
+function clickAlert() {
+    alert('I was clicked!');
+}
+
+input.addEventListener('click', clickAlert);
